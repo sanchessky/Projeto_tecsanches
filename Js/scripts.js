03f38e96e8949267c61edecafaae362f5e8abf8b
Thiago Sanches <thiagomlkgame@gmail.com>
2025-08-01 14:46:39 -0300

    Atualização

--- Js/scripts.js
+++ Js/scripts.js
@@ -11,7 +11,6 @@ function carregar_novidades() {
               <h3>${liv.nome}</h3>
               <p>${liv.preco}</p>
               <a href=detalhes.html?id=${liv.id}>Mais Detalhes</a>
-
             </div>
            `
             })
@@ -19,9 +18,7 @@ function carregar_novidades() {
         })
         .catch((erro) => { console.error(erro) })
     carregar_produtomaisvendidos()
-
 }
-
 let pe = 0
 function rolarNovidadesEsquerda() {
     if (pe < -700) {
@@ -32,10 +29,8 @@ function rolarNovidadesEsquerda() {
     }
     const livrosnovidades = document.getElementById("produtosnovidades");
     livrosnovidades.style.marginLeft = `${pe}px`
-    //testeconsole.log(livrosnovidades.style.marginLeft)
 }
 function rolarNovidadesDireita() {
-
     if (pe > 0) {
         pe = 0
     }
@@ -44,9 +39,7 @@ function rolarNovidadesDireita() {
     }
     const livrosnovidades = document.getElementById("produtosnovidades");
     livrosnovidades.style.marginLeft = `${pe}px`
-    // teste console.log(livrosnovidades.style.marginLeft)
 }
-
 function carregar_produtomaisvendidos() {
     const produtos_maisvendidos = document.getElementById("produtosmaisvendidos");
     let saida = "";
@@ -59,17 +52,13 @@ function carregar_produtomaisvendidos() {
                 <img src="${liv.foto1}" alt="Capa ${liv.nome}">
                 <h3>${liv.nome}</h3>
                 <p class="quantidade">Procurados: ${liv.quantidade}</p>
-                    
             </div>`
             })
             produtos_maisvendidos.innerHTML = saida;
         })
         .catch((erro) => { console.error(erro) })
     carregar_autores()
-
-
 }
-
 function carregar_autores() {
     const produtos_funcionario = document.getElementById("produtosfuncionarios");
     let saida = "";
@@ -86,10 +75,8 @@ function carregar_autores() {
             produtos_funcionario.innerHTML = saida;
         })
         .catch((erro) => { console.error(erro) })
-
     carregar_produtomaisvendidos()
 }
-
 function carregar_pcgamer() {
     const lstlivros = document.getElementById("lstlivros")
     let saida = "";
@@ -108,7 +95,6 @@ function carregar_pcgamer() {
             lstlivros.innerHTML = saida;
         })
         .catch((erro) => { console.error(erro) })
-
 }
 function carregar_hardware() {
     const lstlivros = document.getElementById("lstlivros")
@@ -129,9 +115,6 @@ function carregar_hardware() {
         })
         .catch((erro) => { console.error(erro) })
 }
-
-
-
 function carregar_monitores() {
     const lstlivros = document.getElementById("lstlivros")
     let saida = "";
@@ -211,7 +194,6 @@ function carregar_outros() {
 let nome_carrinho = "carrinho"
 let produto_no_carrinho = localStorage.getItem(nome_carrinho) ? JSON.parse(localStorage.getItem(nome_carrinho)) : [];
 function adicionar_carrinho(id, foto, nome, preco, qtd) {
-
     let produto = {
         id: id,
         nome_produto: nome,
@@ -221,34 +203,24 @@ function adicionar_carrinho(id, foto, nome, preco, qtd) {
     };
     produto_no_carrinho.push(produto);
     console.log(produto_no_carrinho);
-    //add lista de produto do carrinho ao banco de dados do navegador, usando o comando localstorage
     window.localStorage.setItem(nome_carrinho, JSON.stringify(produto_no_carrinho));
-
 };
-
 function carregar_detalhes() {
     let idlivro = window.location.search.split('=');
     idlivro = idlivro[1];
-
     const div_detalhes = document.getElementById("detalhes");
     fetch(`http://127.0.0.1:5000/api/v1/produto/listarporid/${idlivro}`)
         .then((res) => res.json())
         .then((dt) => {
             console.log(dt)
-
             let div_img = document.createElement("div");
             div_img.setAttribute("id", "div_img")
             let div_capa = document.createElement("div");
             div_capa.setAttribute("id", "div_capa")
             let img_capa = document.createElement("img");
             img_capa.src = dt[0].foto1;
-
-            //adicionar a imagem da capa a div capa
             div_capa.appendChild(img_capa);
-            //adicionar a div capa a div imagem
             div_img.appendChild(div_capa);
-
-
             let div_miniatura = document.createElement("div");
             div_miniatura.setAttribute("id", "div_miniatura");
             let img_miniatura1 = document.createElement("img");
@@ -257,78 +229,48 @@ function carregar_detalhes() {
             img_miniatura1.src = dt[0].foto1;
             img_miniatura2.src = dt[0].foto2;
             img_miniatura3.src = dt[0].foto3;
-
-            //adicionar as foto da miniatura dentro da div miniatura
             div_miniatura.appendChild(img_miniatura1);
             div_miniatura.appendChild(img_miniatura2);
             div_miniatura.appendChild(img_miniatura3);
-
-            //colocar a div miniatura dentro da div img
             div_img.appendChild(div_miniatura);
-            //colocar a div de imagem dentro da div detalhes
             div_detalhes.appendChild(div_img)
-
             let div_titulo_descricao = document.createElement("div");
             div_titulo_descricao.setAttribute("id", "div_titulo_descricao");
-
             let h3_titulo = document.createElement("h3");
             h3_titulo.innerHTML = dt[0].nome;
-
             let p_descricao = document.createElement("p");
             p_descricao.innerHTML = dt[0].descricao;
-
-            //add o titulo e descrição dentro da div titulo descricao
             div_titulo_descricao.appendChild(h3_titulo)
             div_titulo_descricao.appendChild(p_descricao)
-
             div_detalhes.appendChild(div_titulo_descricao)
-
-
             let div_carrinho = document.createElement("div");
             div_carrinho.setAttribute("id", "div_carrinho");
-
             let p_preco = document.createElement("p");
             p_preco.innerHTML = dt[0].preco;
-
             let btn_add_carrinho = document.createElement("button");
             btn_add_carrinho.innerHTML = "Adicionar ao carrinho";
-
             btn_add_carrinho.onclick = () => {
                 adicionar_carrinho(dt[0].id, dt[0].foto1, dt[0].nome, dt[0].preco, 1);
                 alert('Produto adicionado ao carrinho!')
             };
-
-            //adicionar o p e btn a div carrinho
             div_carrinho.appendChild(p_preco);
             div_carrinho.appendChild(btn_add_carrinho);
-
             div_detalhes.appendChild(div_carrinho);
-
-
         })
         .catch((error) => {
             console.log(error)
         });
 };
-
 const area_carrinho = document.getElementsByClassName("carrinho")[0]
 const div_qtd_itens = document.createElement("div");
 div_qtd_itens.setAttribute("id", "div_qtd_itens");
 area_carrinho.appendChild(div_qtd_itens);
-
-
-
 function remover_do_carrinho(id) {
     produto_no_carrinho = produto_no_carrinho.filter(item => item.id !== id);
     window.localStorage.setItem(nome_carrinho, JSON.stringify(produto_no_carrinho));
     window.location.reload();
 };
-
-
-
 function carregar_produtos_carrinho() {
-
-
     let produto = window.localStorage.getItem("carrinho");
     if (produto != null) {
         document.getElementById("div_qtd_itens").style.display = "block"
@@ -337,7 +279,6 @@ function carregar_produtos_carrinho() {
     console.log(JSON.parse(produto));
     console.log(JSON.parse(produto).length);
     div_qtd_itens.innerHTML = JSON.parse(produto).length;
-
     const lista_produto_carrinho = document.getElementById("lista_produto_carrinho");
     JSON.parse(produto).map((itens) => {
         let mont = `<div>
@@ -349,39 +290,27 @@ function carregar_produtos_carrinho() {
         <img src="img/lixeira.png" class="btnexcluir" onclick="remover_do_carrinho(${itens.id})">
         </div>
         `
-
         lista_produto_carrinho.innerHTML += mont;
     });
-
 };
-
-
 let aute_usuario = "usuario_autenticado";
-
 if (window.localStorage.getItem(aute_usuario)) {
-
     let us = window.localStorage.getItem(aute_usuario);
     us = JSON.parse(us);
-
     let img_usuario = `<img src=${us.payload.fotousuario} class="img_usuario">`;
     let nome_us = us.payload.nomeusuario;
-
     document.getElementsByClassName("usuario")[0].style.padding = "15px"
     document.getElementsByClassName("usuario")[0].innerHTML = img_usuario + nome_us
 };
-
-
 function abrirCadastro() {
     const modalLogin = bootstrap.Modal.getInstance(document.getElementById('modalLogin'));
     if (modalLogin) modalLogin.hide();
-
     const modalCadastro = new bootstrap.Modal(document.getElementById('modalCadastro'));
     setTimeout(() => modalCadastro.show(), 500);
 }
 function efetuarlogin() {
     const usuario = document.getElementById("txtusuario")
     const senha = document.getElementById("txtpassword")
-
     fetch("http://127.0.0.1:5000/api/v1/login/usuario", {
         method: "POST",
         headers: {
@@ -401,11 +330,7 @@ function efetuarlogin() {
             window.location.reload();
         })
         .catch((erro) => console.error(erro))
-
-
 }
-
-
 function Cadastrar() {
     const nome = document.getElementById("txtNome")
     const email = document.getElementById("txtEmail")
@@ -418,8 +343,6 @@ function Cadastrar() {
     const bairro = document.getElementById("bairro")
     const complemento = document.getElementById("complemento")
     const cep = document.getElementById("cep")
-   
-
     fetch("http://127.0.0.1:5000/api/v1/cliente/cadastrar", {
         method: "POST",
         headers: {
@@ -433,13 +356,13 @@ function Cadastrar() {
             telefone: telefone.value,
             data_nascimento: dataNascimento.value,
             endereco: {
-            id: 1,
-            logradouro: logradouro.value,
-            numero: numero.value,
-            complemento: complemento.value,
-            cep: cep.value,
-            bairro: bairro.value
-        }
+                id: 1,
+                logradouro: logradouro.value,
+                numero: numero.value,
+                complemento: complemento.value,
+                cep: cep.value,
+                bairro: bairro.value
+            }
         })
     })
         .then((rs) => rs.json())
@@ -459,7 +382,5 @@ function Cadastrar() {
             window.location.reload();
         })
         .catch((erro) => console.error(erro))
-        alert('Cadastro realizado com sucesso!')
-
+    alert('Cadastro realizado com sucesso!')
 }
-
